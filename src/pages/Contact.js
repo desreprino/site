232b050ac745030021a47904f0a1f2ef5a2fab86
agusand <amandreacchi@gmail.com>
2021-10-06 11:35:16 -0300

    Contact section added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,19 @@
+import ContactForm from "../components/ContactForm";
+import GoogleMap from "../components/GoogleMap";
+
+import whatsappIcon from "../assets/icons/whatsapp_icon.svg";
+
 const Contact = () => {
-	return <>Contacto</>;
+	return (
+		<section className="contact">
+			<ContactForm />
+			<div className="contact__phoneContainer">
+				<img src={whatsappIcon} alt="whatsapp" className="contact__waIcon" />
+				<strong className="contact__phoneNumber">11-****-****</strong>
+			</div>
+			<GoogleMap />
+		</section>
+	);
 };
 
 export default Contact;
