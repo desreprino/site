543412180efa67c602aa7dd8e84e9589e4f1ead4
agusand <amandreacchi@gmail.com>
2021-10-06 21:25:47 -0300

    Home section finished

--- src/components/Carrousel.js
+++
@@ -1,5 +0,0 @@
-const Carrousel = () => {
-	return <></>;
-};
-
-export default Carrousel;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
+import AboutUsMain from "../components/AboutUsMain";
+import CategoryCardContainer from "../components/CategoryCardContainer";
+import Carousel from "../components/Carousel";
+
 const Home = () => {
-	return <>Home</>;
+	return (
+		<section className="home">
+			<Carousel />
+			<div className="home__container">
+				<AboutUsMain isHome="true" />
+			</div>
+			<div className="home__container">
+				<CategoryCardContainer />
+			</div>
+			<Link to="/productos" className="home__moreProductsLink">
+				<button className="home__moreProductsButton">Más productos</button>
+			</Link>
+		</section>
+	);
 };
 
 export default Home;
